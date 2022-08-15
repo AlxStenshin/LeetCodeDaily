bc17ca1821d8fdacc779c4593c040ff69136ff02
AlxStenshin <alexander.stn@gmail.com>
2022-08-15 09:51:50 +0400

    Day 019: 13. Roman To Integer

---
+++ src/main/java/day_019/RomanToInteger.java
@@ -0,0 +1,51 @@
+package day_019;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 13. Roman to Integer
+ * https://leetcode.com/problems/roman-to-integer/
+ *
+ * Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+ *
+ * Symbol       Value
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+ *
+ * Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+ *
+ * I can be placed before V (5) and X (10) to make 4 and 9.
+ * X can be placed before L (50) and C (100) to make 40 and 90.
+ * C can be placed before D (500) and M (1000) to make 400 and 900.
+ * Given a roman numeral, convert it to an integer.
+ */
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Map<Character, Integer> dictionary = new HashMap<>();
+        dictionary.put('I', 1);
+        dictionary.put('V', 5);
+        dictionary.put('X', 10);
+        dictionary.put('L', 50);
+        dictionary.put('C', 100);
+        dictionary.put('D', 500);
+        dictionary.put('M', 1000);
+
+        int target = 0;
+        int previous = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int number = dictionary.getOrDefault(s.charAt(i), 0);
+            target += (number >= previous) ? number : -number;
+            previous = number;
+        }
+        return target;
+    }
+}
