6c66546c284d6a531f45c2092b4fc2c4c0d0a1fd
AlxStenshin <alexander.stn@gmail.com>
2022-09-28 13:20:53 +0400

    Day 063: 19. Remove Nth Node From End of List

--- src/main/java/utils/list/ListNode.java
+++ src/main/java/utils/list/ListNode.java
@@ -17,4 +17,18 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListNode listNode = (ListNode) o;
+        return val == listNode.val &&
+                Objects.equals(next, listNode.next);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, next);
+    }
 }
---
+++ src/test/java/day_063/RemoveNthNodeFromEndOfListTest.java
@@ -0,0 +1,34 @@
+package day_063;
+
+import org.junit.jupiter.api.Test;
+import utils.list.ListNode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveNthNodeFromEndOfListTest {
+
+    @Test
+    void shouldRemove4() {
+        ListNode input = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+        ListNode expected = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(5))));
+
+        assertEquals(expected, new RemoveNthNodeFromEndOfList().removeNthFromEnd(input, 2));
+
+    }
+
+    @Test
+    void shouldRemoveTheOnlyElement() {
+        ListNode input = new ListNode(1);
+        ListNode expected = new ListNode();
+
+        assertEquals(expected, new RemoveNthNodeFromEndOfList().removeNthFromEnd(input, 1));
+    }
+
+    @Test
+    void shouldRemoveFirst() {
+        ListNode input = new ListNode(1, new ListNode(2));
+        ListNode expected = new ListNode(1);
+
+        assertEquals(expected, new RemoveNthNodeFromEndOfList().removeNthFromEnd(input, 1));
+    }
+}
