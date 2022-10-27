e401ea7eb178d4762088485faf0f740ee36a7483
AlxStenshin <alexander.stn@gmail.com>
2022-10-27 10:26:02 +0400

    Day 092: 835. Image Overlap

---
+++ src/test/java/day_092/ImageOverlapTest.java
@@ -0,0 +1,30 @@
+package day_092;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ImageOverlapTest {
+
+    @Test
+    void shouldReturnThree() {
+        assertEquals(3, new ImageOverlap().largestOverlap(
+                new int[][]{{1, 1, 0}, {0, 1, 0}, {0, 1, 0}},
+                new int[][]{{0, 0, 0}, {0, 1, 1}, {0, 0, 1}}));
+    }
+
+    @Test
+    void shouldReturnOne() {
+        assertEquals(1, new ImageOverlap().largestOverlap(
+                new int[][]{{1}},
+                new int[][]{{1}}));
+    }
+
+    @Test
+    void shouldReturnZero() {
+        assertEquals(0, new ImageOverlap().largestOverlap(
+                new int[][]{{0}},
+                new int[][]{{0}}));
+    }
+
+}
