14ce9acadcbc951746d3d3f7b65c3eb08dd103b8
AlxStenshin <alexander.stn@gmail.com>
2023-03-10 10:16:35 +0400

    Day 226: Javadoc

--- src/main/java/day_226/LinkedListRandomNodeReservoirSampling.java
+++ src/main/java/day_226/LinkedListRandomNodeReservoirSampling.java
@@ -2,6 +2,18 @@ package day_226;
 
 import utils.list.ListNode;
 
+/**
+ * <a href = "https://leetcode.com/problems/linked-list-random-node/">
+ * 382. Linked List Random Node </a>
+ * <p>
+ * Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
+ * <p>
+ * Implement the Solution class:
+ * <p>
+ * Solution(ListNode head) Initializes the object with the head of the singly-linked list head.<br>
+ * int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
+ */
+
 public class LinkedListRandomNodeReservoirSampling {
     private final ListNode head;
 
--- src/test/java/day_226/LinkedListRandomNodeReservoirSamplingTest.java
+++ src/test/java/day_226/LinkedListRandomNodeReservoirSamplingTest.java
@@ -8,18 +8,6 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * <a href = "https://leetcode.com/problems/linked-list-random-node/">
- * 382. Linked List Random Node </a>
- * <p>
- * Given a singly linked list, return a random node's value from the linked list. Each node must have the same probability of being chosen.
- * <p>
- * Implement the Solution class:
- * <p>
- * Solution(ListNode head) Initializes the object with the head of the singly-linked list head.<br>
- * int getRandom() Chooses a node randomly from the list and returns its value. All the nodes of the list should be equally likely to be chosen.
- */
-
 class LinkedListRandomNodeReservoirSamplingTest {
 
     @Test
